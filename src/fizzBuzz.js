29c410d06400e141d7dc4849b521c2ab07b72ca1
lizzkats <katsnelson.lizz@gmail.com>
2017-01-04 14:02:50 -0800

    Adds fizzbuzz algorithm and tests

---
+++ src/fizzBuzz.js
@@ -0,0 +1,23 @@
+'use strict'
+
+export default function fizzBuzz() {
+  var arrayNums = [];
+  for (var i = 1; i < 101; i++){
+    arrayNums.push(i)
+  }
+
+  for(var i = 1; i < arrayNums.length; i++) {
+    if (arrayNums[i] % 3 == 0 ){
+      arrayNums.splice(i, 1, "Fizz");
+  }
+  if (arrayNums[i] % 5 == 0){
+    arrayNums.splice(i, 1, "Buzz");
+  }
+  if (i % 15 == 0 ){
+    arrayNums.splice(i -1 , 1, "FizzBuzz");
+  }
+  }
+  return arrayNums;
+
+
+  }
--- src/makeChange.js
+++ src/makeChange.js
@@ -1,5 +1,38 @@
 'use strict'
 
 export default function makeChange({price, amountGiven}) {
-  // your code here
-}
+  var looseChange = {
+  quarters: 0,
+  dimes: 0,
+  nickels: 0,
+  pennies: 0
+  };
+
+  var change = (amountGiven - price) * 100;
+
+  if(change < 0){
+  return "Not enough money";
+  }
+
+  var quarters = Math.floor(change / 25);
+  looseChange.quarters += quarters;
+  var remainder = change - quarters * 25;
+
+  var dimes = Math.floor(remainder / 10);
+  looseChange.dimes += dimes;
+  remainder = remainder - dimes * 10
+
+  var nickels = Math.floor(remainder / 5);
+  looseChange.nickels += nickels;
+  remainder = remainder - nickels * 5;
+
+  var pennies = Math.floor(remainder);
+  looseChange.pennies = pennies;
+
+  return looseChange;
+  }
+
+  //in an object
+  //takes a decimal value and makes change based on price
+  //breaks it into denominations of quarters, nickels, dimes and pennies
+  // will return a key value pair according to higher order value
